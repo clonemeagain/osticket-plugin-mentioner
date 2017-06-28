b2581c0e063b29b7a93c6a1a593be95945812a99
Aaron Were <awere@tdj.com.au>
2017-06-29 08:44:29 +1000

    Updating

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Parses messages for Mentions, and adds users/agents emails or names as collaborators on a ticket.
 
 Concept pinched as lazily as possible from: http://osticket.com/forum/discussion/comment/115623
+
+Checkout into /include/plugins/mentioner and Install as per normal osTicket Plugins
+
+Now new thread entries (mail/messages/notes) will be checked for words starting with an @ symbol and have those words checked for Agent's usernames.
+
--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php return array(
  'id' => 'clonemeagain:mentioner', # notrans
- 'version' => '1',
+ 'version' => '1.1',
  'name' => 'Mentioner',
  'author' => 'clonemeagain@gmail.com',
- 'description' => 'Notices @mentions and adds collaborators.',
+ 'description' => 'Notices @mentions and those names as collaborators.',
  'url' => 'https://github.com/clonemeagain/osticket-plugin-mentioner',
  'plugin' => 'class.MentionerPlugin.php:MentionerPlugin'
 );
