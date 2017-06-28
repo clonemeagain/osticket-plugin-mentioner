d51b05339d6500a3f51d865ec2fc322823663da4
Aaron Were <awere@tdj.com.au>
2017-06-29 09:54:46 +1000

    Adding email matching relates to #1 and #2

--- class.MentionerPlugin.php
+++ class.MentionerPlugin.php
@@ -1,13 +1,20 @@
 <?php
 require_once (INCLUDE_DIR . 'class.signal.php');
+require_once ('config.php');
 
 /**
  * The goal of this Plugin is to read messages, find things that look like: @Grizly and add Agent Grizly to the collaborators, or things like @User and add that user
  *
- * TODO: use @email@address.com it will find that user/agent via address lookup, strip the first @ and add them as a a collaborator.
+ * Checks @email prefix for admin-defined domain.com it will find that user/agent via address lookup, then add them as a a collaborator.
  */
 class MentionerPlugin extends Plugin {
-	const DEBUG = FALSE;
+	/**
+	 * Which config to use (in config.php)
+	 *
+	 * @var string
+	 */
+	public $config_class = 'MentionerPluginConfig';
+	const MAX_LENGTH_NAME = 128;
 	
 	/**
 	 * Run on every instantiation of osTicket..
@@ -19,46 +26,93 @@ class MentionerPlugin extends Plugin {
 	 */
 	function bootstrap() {
 		Signal::connect ( 'threadentry.created', function (ThreadEntry $entry) {
-			// Get the contents of the ThreadEntryBody to check the text
-			$text = $entry->getBody ()->getClean ();
+			$this->checkThreadTextForMentions ( $entry );
+		} );
+	}
+	
+	/**
+	 * Function was getting a bit long to be lambda..
+	 *
+	 * @param ThreadEntry $entry        	
+	 */
+	private function checkThreadTextForMentions(ThreadEntry $entry) {
+		// Get the contents of the ThreadEntryBody to check the text
+		$text = $entry->getBody ()->getClean ();
+		
+		// Initial check for the @ symbol..
+		if (strpos ( $text, '@' ) !== FALSE) {
+			// We've an @.. let's check it for Mentions!
+			$matches = array ();
 			
-			// Initial check for the @ symbol..
-			if (strpos ( $text, '@' ) !== FALSE) {
-				// We've an @.. let's check it for Mentions!
-				$matches = array ();
-				
-				// regex pinched from: https://stackoverflow.com/a/10384251/276663
-				if (preg_match_all ( '/(^|\s)(@\w+)/i', $text, $matches ) !== FALSE) {
-					if (count ( $matches [2] )) {
-						$mentions = array_unique ( $matches [2] );
-						foreach ( $mentions as $idx => $name ) {
-							$name = ltrim ( $name, '@' );
+			// regex pinched from: https://stackoverflow.com/a/10384251/276663
+			if (preg_match_all ( '/(^|\s)(@\w+)/i', $text, $matches ) !== FALSE) {
+				if (count ( $matches [2] )) {
+					$mentions = array_unique ( $matches [2] );
+					
+					// Fetch admin config:
+					$c = $this->getConfig (); // (PluginConfig)
+					$match_staff_only = $c->get ( 'agents-only' ); // (bool)
+					$email_domain = $c->get ( 'email-domain' ); // (string)
+					
+					foreach ( $mentions as $idx => $name ) {
+						$name = ltrim ( $name, '@' ); // remove @ prefix
+						$name = substr ( $name, 0, self::MAX_LENGTH_NAME ); // restrict the length of $name, prevent overflow
+						
+						if (! $name)
+							continue;
+						
+						// TODO: Someone might want many domains here..
+						if ($email_domain) {
+							$email = "$name@$email_domain";
 							
-							// Check for Staff with that name: Can you even add staff? Nope.. only Users.
-							$s = Staff::lookup ( $name );
+							// Trigger Staff match via the Email Validator (third)
+							$staff = Staff::lookup ( $email );
 							
-							if ($s instanceof Staff) {
-								// Craft a User to match the Staff account:
-								$vars = array (
-										'name' => $name,
-										'email' => $s->getEmail () 
-								);
-								$o = User::fromVars ( $vars, true );
-							} else {
-								// It's not a Staff/Agent account, maybe it's a user, or a Staff-User we've previously created:
-								$o = User::lookup ( $name );
+							// Email might be for a User not Agent
+							if (! $staff && ! $match_staff_only) {
+								$user = User::lookupByEmail ( $email );
+								if ($user instanceof User) {
+									$this->addCollaborator ( $entry, $user );
+									continue;
+								}
 							}
-							
-							if ($o) {
-								// Attempt to add the collaborator to the thread (won't duplicate by default)
-								$vars = $errors = array ();
-								$entry->getThread ()->addCollaborator ( $o, $vars, $errors, true );
-							}
+						}
+						
+						// Check for Staff with that name (skip if email matched)
+						if (! $staff)
+							$staff = Staff::lookup ( $name );
+						
+						if ($staff instanceof Staff) {
+							// Craft a User to match the Staff account:
+							$vars = array (
+									'name' => $name,
+									'email' => $staff->getEmail () 
+							);
+							$user = User::fromVars ( $vars, true );
+						} elseif (! $match_staff_only) {
+							// It's not a Staff/Agent account, maybe it's a user, or a Staff-User we've previously created:
+							$user = User::lookup ( $name );
+						}
+						
+						if ($user instanceof User) {
+							$this->addCollaborator ( $entry, $user );
 						}
 					}
 				}
 			}
-		} );
+		}
+	}
+	
+	/**
+	 * Adds a collaborating User to the ThreadEntry
+	 *
+	 * @param ThreadEntry $entry        	
+	 * @param User $user        	
+	 */
+	private function addCollaborator(ThreadEntry $entry, User $user) {
+		// Attempt to add the collaborator to the thread (won't duplicate by default)
+		$vars = $errors = array ();
+		$entry->getThread ()->addCollaborator ( $user, $vars, $errors, true );
 	}
 	
 	/**
---
+++ config.php
@@ -0,0 +1,49 @@
+<?php
+require_once INCLUDE_DIR . 'class.plugin.php';
+class MentionerPluginConfig extends PluginConfig {
+	// Provide compatibility function for versions of osTicket prior to
+	// translation support (v1.9.4)
+	function translate() {
+		if (! method_exists ( 'Plugin', 'translate' )) {
+			return array (
+					function ($x) {
+						return $x;
+					},
+					function ($x, $y, $n) {
+						return $n != 1 ? $y : $x;
+					} 
+			);
+		}
+		return Plugin::translate ( 'mentioner' );
+	}
+	
+	/**
+	 * Build an Admin settings page.
+	 *
+	 * {@inheritdoc}
+	 *
+	 * @see PluginConfig::getOptions()
+	 */
+	function getOptions() {
+		list ( $__, $_N ) = self::translate ();
+		return array (
+				'sb1' => new SectionBreakField ( [ 
+						'label' => 'Who can be mentioned?' 
+				] ),
+				'agents-only' => new BooleanField ( [ 
+						'label' => $__ ( 'Only allow mentions from Agents' ) 
+				] ),
+				'sb' => new SectionBreakField ( [ 
+						'label' => $__ ( 'Match email addresses?' ),
+						'hint' => $__ ( 'If you put domain.com here, we\'ll try and match @user as user@domain.com and lookup their account.' ) 
+				] ),
+				'email-domain' => new TextboxField ( [ 
+						'label' => $__ ( 'Which domains to match emails for?' ),
+						'configuration' => array (
+								'size' => 40,
+								'length' => 256 
+						) 
+				] ) 
+		);
+	}
+}
